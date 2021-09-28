3e3930649bbd51e3baf1e07f0e745e697e543e4c
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-09-28 16:11:00 +0100

    Added new Footer component and CSS file to support it

---
+++ src/FooterContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./FooterContent.css";
+
+
+export default function FooterContent() {
+  return (
+    <div className="footer">
+      <small>
+        Coded by <a href="https://www.linkedin.com/in/julina-shrestha-58a89a109/">Julina Shrestha</a>
+      </small>
+    </div>
+  )
+}
