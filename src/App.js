0362a5e1213c37f8306c90f9f4b232a5e77bbf43
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-09-29 16:12:19 +0100

    Added Icons

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import FooterContent from './FooterContent';
 function App() {
   return (
     <div className="App">
+      <div class="bg"></div>
+      <div class="bg bg2"></div>
+      <div class="bg bg3"></div>
       <header className="App-header">
-        Dictionary App
+        Dictionary for you <img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-book-back-to-school-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png" alt="" /><img src="https://img.icons8.com/office/60/000000/anime-emoji.png" alt=""/>
       </header>
       <main>
         <Dictionary />
