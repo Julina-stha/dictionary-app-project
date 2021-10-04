ca190910831b98feaf4b1431ce2e8dd727824ad2
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-10-04 16:21:41 +0100

    added ClassNames to style the content

--- src/Result.js
+++ src/Result.js
@@ -1,27 +1,33 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import "./App.css";
 
 
 export default function Result(props) {
   if (props.resultData) {
     return (
       <div className="results">
-        <h3>You are looking for the word <span className="text-capitalize"><strong>{props.resultData.word}</strong></span></h3>
-        {props.resultData.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          )
-        })}
+        <div className="phonetics-part">
+          <section>
+            <h3>You are looking for the word:
+            <div className="text-capitalize" id="KeyWord"><strong>{props.resultData.word}</strong></div></h3>
+            {props.resultData.phonetics.map(function (phonetic, index) {
+              return (
+                <div className="phonetic-button" key={index}>
+                  <Phonetic phonetic={phonetic} />
+                </div>
+              )
+            })}
+          </section>
+        </div>
         {props.resultData.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning}/>
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning}/>
             </div>
-          )
-        })}
+            )
+          })}
       </div>
   );
   } else {
