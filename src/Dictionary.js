e22b4d16a882561338c41ef76858e1cb5cf1427a
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-10-04 16:20:18 +0100

    Added Header element, edited h2 and imported new CSS file

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
+import "./Dictionary.css";
+
 
 export default function Dictionary() {
   const [keyWord, setKeyWord] = useState("");
@@ -22,7 +24,10 @@ export default function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <h2>What word are you looking for? <img src="https://img.icons8.com/ios/100/000000/thinking-bubble.png" className="cloud" alt="" width="60" /></h2>
+      <header className="App-header">
+        Dictionary for you! <img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-book-back-to-school-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png" alt="" />
+      </header>
+      <h2>What word are you looking for today? <img src="https://img.icons8.com/ios/100/000000/thinking-bubble.png" className="cloud" alt="" width="50" /></h2>
       <form onSubmit={search}>
         <input type="search" className="search-engine" onChange={recordTypedWord} autoComplete="off"></input><img src="https://img.icons8.com/cotton/45/000000/search--v2.png" className="search-icon" width="45" alt=""/>
       </form>
