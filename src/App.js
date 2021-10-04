4761ee17459e80db29d227e617c7042efd4167f5
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-10-04 16:19:34 +0100

    Moved the header element to other Component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
       <div class="bg"></div>
       <div class="bg bg2"></div>
       <div class="bg bg3"></div>
-      <header className="App-header">
-        Dictionary for you <img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-book-back-to-school-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png" alt="" /><img src="https://img.icons8.com/office/60/000000/anime-emoji.png" alt=""/>
-      </header>
       <main>
         <Dictionary />
       </main>
