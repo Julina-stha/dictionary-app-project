c7d42ee4f8b3fdac2eb68c5d410857608da3eea3
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-10-05 16:12:10 +0100

    Integrated Pexel API to the component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 
 export default function Dictionary() {
   const [keyWord, setKeyWord] = useState("");
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelApiKey = "563492ad6f91700001000001c2a803b5557b4a3d8a651498a8ef2244";
+    let pexelUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=9`;
+    axios.get(pexelUrl, { headers: { Authorization : `Bearer ${pexelApiKey}` }}).then(handlePexelResponse);
   }
 
-  function handleResponse(response) {
+  function handlePexelResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
+  }
+
+
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
@@ -31,7 +43,8 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" className="search-engine" onChange={recordTypedWord} autoComplete="off"></input><img src="https://img.icons8.com/cotton/45/000000/search--v2.png" className="search-icon" width="45" alt=""/>
       </form>
-      <Result resultData={results} searchWord={keyWord}/>
+      <Result resultData={results} searchWord={keyWord} />
+      <Photos photoData={photos} />
     </div>
   )
 }
