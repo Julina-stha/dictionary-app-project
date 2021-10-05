766ba7a9d5ecba79e3688b7f1a0c01f9b45335dd
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-10-05 20:30:20 +0100

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -4,20 +4,21 @@ import "./Photos.css";
 export default function Photos(props) {
 
   if (props.photoData) {
-  return (
-    <section className="Photos">
-      <div className="row">
-        {props.photoData.map(function (photos, index) {
-          return (
-            <div className="col-4" key={index}>
-              <a href={photos.url} target="_blank" rel="noreferrer">
-                <img src={photos.src.landscape} className="img-fluid" alt="" />
-              </a>
-            </div>    
-          )
-        })}
-      </div>
-    </section>)
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photoData.map(function (photos, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photos.url} target="_blank" rel="noreferrer">
+                  <img src={photos.src.landscape} className="img-fluid" alt="" />
+                </a>
+              </div>    
+            )
+          })}
+        </div>
+      </section>
+  )
   } else {
     return (null);
   }
