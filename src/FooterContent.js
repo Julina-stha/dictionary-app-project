1163888d09594c126fb0afa2ecc379ceebd9a325
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-10-05 16:12:40 +0100

    Added more content to the Footer

--- src/FooterContent.js
+++ src/FooterContent.js
@@ -6,7 +6,7 @@ export default function FooterContent() {
   return (
     <div className="footer">
       <small>
-        Coded by <a href="https://www.linkedin.com/in/julina-shrestha-58a89a109/">Julina Shrestha</a>
+        Coded by <a href="https://www.linkedin.com/in/julina-shrestha-58a89a109/" target="_blank" rel="noreferrer" className="Coder">Julina Shrestha</a> opensourced on <a href="https://github.com/Julina-stha/dictionary-app-project" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a> and hosted on <a href="https://www.netlify.com/" target="_blank" rel="noreferrer"><img src="https://seeklogo.com/images/N/netlify-logo-758722CDF4-seeklogo.com.png" alt="Netlify"></img></a>
       </small>
     </div>
   )
