c0cd8c2e08d88f39f048dbb66bbdf794d748dcde
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-02-14 13:00:56 +0000

    updated CSS to make the site responsive

--- src/FooterContent.js
+++ src/FooterContent.js
@@ -6,7 +6,7 @@ export default function FooterContent() {
   return (
     <div className="footer">
       <small>
-        👩‍💻Coded by <a href="https://www.linkedin.com/in/julina-shrestha-58a89a109/" target="_blank" rel="noreferrer" className="coder">Julina Shrestha</a><br></br> Opensourced on <a href="https://github.com/Julina-stha/dictionary-app-project" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a> and hosted on <a href="https://www.netlify.com/" target="_blank" rel="noreferrer"><img src="https://seeklogo.com/images/N/netlify-logo-758722CDF4-seeklogo.com.png" alt="Netlify"></img></a>
+        👩‍💻 Coded by <a href="https://www.linkedin.com/in/julina-shrestha-58a89a109/" target="_blank" rel="noreferrer" className="coder">Julina Shrestha</a><br></br> Opensourced on <a href="https://github.com/Julina-stha/dictionary-app-project" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a> and hosted on <a href="https://www.netlify.com/" target="_blank" rel="noreferrer"><img src="https://seeklogo.com/images/N/netlify-logo-758722CDF4-seeklogo.com.png" alt="Netlify"></img></a>
       </small>
     </div>
   )
