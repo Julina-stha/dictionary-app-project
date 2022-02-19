0146058c3976a0c259875cbefed5d5b92dffccef
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2022-02-19 18:19:02 +0000

    Fixed an error caused by API

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -12,8 +12,9 @@ export default function Phonetic(props) {
     return (
       <div className="Phonetic">
         <button type="button" onClick={handleClick}><img src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/50/000000/external-speaker-ux-and-ui-flatart-icons-lineal-color-flatarticons.png" alt=""/></button>
-        <br />
-        {props.phonetic.text}
+        <div className="phonetic-text">
+          {props.phonetic.text}
+        </div>
       </div>
     );
 
